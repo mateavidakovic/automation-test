c2a67eec2a8fdf205ca72f9be4743f1424bd6441
Matea Vidakovic <mateavidakovic20@gmail.com>
2025-12-11 11:47:13 +0100

    Remove temporary visual screenshot test

--- tests/demoblaze-home-visual.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Demoblaze home screenshot', async ({ page }) => {
-    await page.goto('https://www.demoblaze.com/index.html');
-    await expect(page).toHaveScreenshot();
-})
