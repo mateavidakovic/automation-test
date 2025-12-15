cc39b5facf73f60628f157d0ca5e937a56d50fd4
Matea Vidakovic <mateavidakovic20@gmail.com>
2025-12-15 15:20:50 +0100

    Add Contact modal test

--- tests/demoblaze-contactpage.spec.ts
+++ tests/demoblaze-contactpage.spec.ts
@@ -9,3 +9,13 @@ test('Contact modal opens', async ({ page }) => {
   await expect(page.locator('#recipient-name')).toBeVisible();
   await expect(page.locator('#message-text')).toBeVisible();
 });
+
+test('Contact modal closes', async ({ page }) => {
+  await page.goto('https://www.demoblaze.com/index.html');
+
+  await page.getByRole('link', { name: 'Contact' }).click();
+
+  await expect(page.locator('#exampleModal')).toBeVisible();
+  await page.locator('#exampleModal .btn.btn-secondary').click();
+  await expect(page.locator('#exampleModal')).toBeHidden();
+});
