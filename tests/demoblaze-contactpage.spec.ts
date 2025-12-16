4c305af19e0038ce1feda487e2468a2a078b9d63
Matea Vidakovic <mateavidakovic20@gmail.com>
2025-12-16 09:53:29 +0100

    Add Contact modal fields test

--- tests/demoblaze-contactpage.spec.ts
+++ tests/demoblaze-contactpage.spec.ts
@@ -26,4 +26,20 @@ test('Contact modal has correct title', async ({ page }) => {
     await page.getByRole('link', { name: 'Contact' }).click();
 
     await expect(page.getByText('New message')).toBeVisible();
-})
+});
+
+test('Contact modal fields are correct', async ({ page }) => {
+    await page.goto('https://www.demoblaze.com/index.html');
+
+    await page.getByRole('link', { name: 'Contact' }).click();
+
+    const emailInput = page.locator('#recipient-email');
+    const nameInput = page.locator('#recipient-name');
+    const messageBox = page.locator('#message-text');
+
+    await expect(emailInput).toHaveAttribute('id', 'recipient-email');
+    await expect(nameInput).toHaveAttribute('id', 'recipient-name');
+    await expect(messageBox).toHaveAttribute('id', 'message-text');
+
+});
+
